9205df9c85d0c37fe89b3fd7d44c034eeecc53bd
igokhul <gokhul.tg@proton.me>
2023-10-18 21:01:47 +0530

    one qn

---
+++ SolvedProblems/CPfor100Days/BinarySearch/SearchInRotatedArrayII.java
@@ -0,0 +1,52 @@
+package BinarySearch;
+
+public class SearchInRotatedArrayII {
+    public static void main(String[] args) {
+        int[] nums = { 3, 5, 1 };
+        int target = 1;
+        System.out.println(search(nums, target));
+    }
+
+    public static boolean search(int[] nums, int target) {
+        return helper(nums, 0, nums.length - 1, target);
+    }
+
+    private static boolean helper(int[] nums, int start, int end, int target) {
+        if (start > end) {
+            return false;
+        }
+
+        int mid = start + (end - start) / 2;
+        if (nums[mid] == target) {
+            return true;
+        }
+
+        // Increasing part
+        if (nums[start] > nums[mid]) {
+            if (nums[mid] < target && target <= nums[end]) {
+                // Move right
+                return helper(nums, mid + 1, end, target);
+            } else {
+                // Move left
+                return helper(nums, start, mid - 1, target);
+            }
+        }
+
+        // Decreasing part
+        else if (nums[start] < nums[mid]) {
+            if (nums[start] <= target && target < nums[mid]) {
+                // Move left
+                return helper(nums, start, mid - 1, target);
+            } else {
+                // Move right
+                return helper(nums, mid + 1, end, target);
+            }
+        }
+
+        // Handling duplicates
+        else {
+            // It can be either in left or right
+            return helper(nums, start, mid - 1, target) || helper(nums, mid + 1, end, target);
+        }
+    }
+}
---
+++ SolvedProblems/CPfor100Days/DivideTwoIntegers.java
@@ -0,0 +1,42 @@
+public class DivideTwoIntegers {
+    public static void main(String[] args) {
+        int dividend = -15;
+        int divisor = -3;
+        System.out.println(divide(dividend, divisor));
+    }
+
+    public static int divide(int dividend, int divisor) {
+        if (dividend > Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        }
+
+        if (dividend < Integer.MIN_VALUE) {
+            return Integer.MIN_VALUE;
+        }
+
+        if (dividend == Integer.MIN_VALUE) {
+            if (divisor == 1) {
+                return Integer.MIN_VALUE;
+            }
+            if (divisor == -1) {
+                return Integer.MAX_VALUE;
+            }
+        }
+
+        boolean negative = ((dividend < 0) == (divisor < 0)) ? false : true;
+        dividend = Math.abs(dividend);
+        divisor = Math.abs(divisor);
+
+        int result = 0;
+        while (dividend - divisor >= 0) {
+            int count = 0;
+            while (dividend - (divisor << 1 << count) >= 0) {
+                count++;
+            }
+            dividend -= divisor << count;
+            result += 1 << count;
+        }
+
+        return (negative) ? -result : result;
+    }
+}
