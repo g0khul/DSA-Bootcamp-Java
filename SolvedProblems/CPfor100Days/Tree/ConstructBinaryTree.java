a597da705cfa2040882b71fa7153bbca52c42642
igokhul <gokhul.tg@proton.me>
2023-12-14 09:46:37 +0530

    Refactor tree construction using a better approach

--- SolvedProblems/CPfor100Days/Tree/ConstructBinaryTree.java
+++ SolvedProblems/CPfor100Days/Tree/ConstructBinaryTree.java
@@ -1,10 +1,42 @@
 package CPfor100Days.Tree;
 
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.HashMap;
 
 public class ConstructBinaryTree {
+    public static TreeNode buildTreeBetterWay(int[] preorder, int[] inorder) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+
+        int[] preIndex = { 0 };
+
+        return helper(preorder, inorder, 0, inorder.length - 1, map, preIndex);
+    }
+
+    public static TreeNode helper(int[] preorder, int[] inorder, int start, int end, HashMap<Integer, Integer> map,
+            int[] preIndex) {
+        if (start > end) {
+            return null;
+        }
+
+        int currIndex = preIndex[0];
+
+        TreeNode node = new TreeNode(preorder[currIndex]);
+        preIndex[0]++;
+
+        if (start == end) {
+            return node;
+        }
+
+        node.left = helper(preorder, inorder, start, map.get(preorder[currIndex]) - 1, map, preIndex);
+        node.right = helper(preorder, inorder, map.get(preorder[currIndex]) + 1, end, map, preIndex);
+
+        return node;
+    }
+
     public static TreeNode buildTree(int[] preorder, int[] inorder) {
         if (preorder.length == 0) {
             return null;
@@ -27,9 +59,9 @@ public class ConstructBinaryTree {
     }
 
     public static void main(String[] args) {
-        int[] preorder = { 3, 9, 20, 15, 7 };
-        int[] inorder = { 9, 3, 15, 20, 7 };
-        TreeNode root = buildTree(preorder, inorder);
+        int[] preorder = { 1, 2, 4, 8, 9, 5, 3, 6, 7 };
+        int[] inorder = { 8, 4, 9, 2, 5, 1, 6, 3, 7 };
+        TreeNode root = buildTreeBetterWay(preorder, inorder);
         root.prettyDisplay(root);
     }
 }
@@ -51,30 +83,6 @@ class TreeNode {
         this.val = val;
         this.left = left;
         this.right = right;
-    }
-
-    // Insert method to insert values in tree in level order
-    public void insert(int value) {
-        TreeNode node = new TreeNode(value);
-        if (this.val == 0) {
-            this.val = value;
-            return;
-        }
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(this);
-        while (!queue.isEmpty()) {
-            TreeNode current = queue.poll();
-            if (current.left == null) {
-                current.left = node;
-                break;
-            } else if (current.right == null) {
-                current.right = node;
-                break;
-            } else {
-                queue.add(current.left);
-                queue.add(current.right);
-            }
-        }
     }
 
     // Display method to print tree in tree format
